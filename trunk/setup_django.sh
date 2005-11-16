c7c605c897a4c2eabbd7c29bb7120e466f2e07f2
gecko <gecko@6a0ee764-e903-0410-862f-d6150147bfc8>
2005-11-16 23:42:19 +0000

    Add short setup script ot make using Django easier

---
+++ trunk/setup_django.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export PATH=`pwd`/support/django_src/django/bin:$PATH
+export DJANGO_SETTINGS_MODULE=gappy.settings
+export PYTHONPATH=`pwd`/..:$PYTHONPATH
+if [ $HOST != 'zippy.dorm.duke.edu' ]; then
+	export PYTHONPATH=`pwd`/support/django_src/django:$PYTHONPATH
+fi
