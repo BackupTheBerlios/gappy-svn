a409183a77acf6f6b4fd6b8a39d0625d1effb860
gecko <gecko@6a0ee764-e903-0410-862f-d6150147bfc8>
2005-11-17 00:07:56 +0000

    Fixed a slight typo in the setup script

--- trunk/setup_django.sh
+++ trunk/setup_django.sh
@@ -2,7 +2,7 @@
 
 export PATH=`pwd`/support/django_src/django/bin:$PATH
 export DJANGO_SETTINGS_MODULE=gappy.settings
-export PYTHONPATH=`pwd`/..:$PYTHONPATH
+export PYTHONPATH=`pwd`:$PYTHONPATH
 if [ $HOST != 'zippy.dorm.duke.edu' ]; then
 	export PYTHONPATH=`pwd`/support/django_src/django:$PYTHONPATH
 fi
