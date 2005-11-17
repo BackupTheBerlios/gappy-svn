83bf288897544411e291e64d81264a5c7c6bf2f7
nebflow85 <nebflow85@6a0ee764-e903-0410-862f-d6150147bfc8>
2005-11-17 00:53:22 +0000

    update to setup shell script, fixed problem with PYTHONPATH

--- trunk/setup_django.sh
+++ trunk/setup_django.sh
@@ -3,6 +3,4 @@
 export PATH=`pwd`/support/django_src/django/bin:$PATH
 export DJANGO_SETTINGS_MODULE=gappy.settings
 export PYTHONPATH=`pwd`:$PYTHONPATH
-if [ $HOST != 'zippy.dorm.duke.edu' ]; then
-	export PYTHONPATH=`pwd`/support/django_src/django:$PYTHONPATH
-fi
+export PYTHONPATH=`pwd`/support/django_src:$PYTHONPATH
